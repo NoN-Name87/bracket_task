3fcffc77c14aca5e92463be020ebcd27b7fbaaf4
NoN-Name <metaldegid02@gmail.com>
2024-02-20 20:52:52 +0300

    feat: another solution

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <cstring>
+#include <stack>
+#include <algorithm>
+#include <map>
 
 constexpr char SQUO_BRACKET = '[';
 constexpr char FIGO_BRACKET = '{';
@@ -9,6 +12,9 @@ constexpr char FIGC_BRACKET = '}';
 constexpr char ROUC_BRACKET = ')';
 constexpr char ERROR = '!';
 constexpr char SPACE = ' ';
+constexpr char EMPTY = 'E';
+
+using Brackets = std::map<char, std::stack<char>>; 
 
 char get_close_bracket(char bracket)
 {
@@ -30,49 +36,57 @@ char get_close_bracket(char bracket)
     return brack_type;
 }
 
-int is_bracket_paired(const std::string & sqcs, size_t index, char brack_type) // checked only one sequence
+bool is_brackets_paired(const std::string & sqcs)
 {
-    for(index; index < sqcs.size(); index++)
+    Brackets bracks;
+    char curr_brack{ERROR};
+    for(const char & i : sqcs)
     {
-        char bracket = get_close_bracket(sqcs[index]);
-        if(sqcs[index] == SPACE)
+        if(i == SPACE)
             continue;
-        if(sqcs[index] == brack_type)
-            break;
-        else if(bracket != ERROR)
-            index = is_bracket_paired(sqcs, index + 1, bracket);
-        else
-            index = sqcs.size();
-    }
-    return index;
-}
-
-void check_all_sequence(const std::string & sqcs)
-{
-    for(size_t i = 0; i < sqcs.size(); i++)
-    {
-        char bracket{};
-        bracket = get_close_bracket(sqcs[i]);
-        if(sqcs[i] == SPACE)
-            continue;
-        if(bracket != ERROR)
-            i = is_bracket_paired(sqcs, i + 1, bracket);
-        if(i >= sqcs.size() || bracket == ERROR)
+        char temp = get_close_bracket(i);
+        if(temp != ERROR)
         {
-            std::cout << "no\n";
-            return;
+            curr_brack = temp;
+            bracks[curr_brack].push(i);
         }
+        else if((curr_brack == EMPTY || curr_brack == i) && bracks[i].size())
+        {
+            bracks[i].pop();
+            curr_brack = EMPTY;    
+        }
+        else
+            return false;
     }
-    std::cout << "yes\n";
+    auto iter = std::find_if(bracks.cbegin(), bracks.cend(), [](auto & brack){ return brack.second.size(); });
+    return iter == bracks.cend();
 }
 
 int main(int argc, char **argv)
 {
     std::string sqcs{""};
-    for(size_t i = 1; i < argc; i++)
+    try
     {
-        sqcs += std::string(argv[i]);
+        if(argc > 1)
+        {
+            for(size_t i = 1; i < argc; i++)
+            {
+                sqcs += std::string(argv[i]);
+            }
+        }
+        else
+        {
+            std::cout << "Enter bracket sequence:\n";
+            std::getline(std::cin, sqcs); 
+        }
+        if(is_brackets_paired(sqcs))
+            std::cout << "yes\n";
+        else
+            std::cout << "no\n"; 
     }
-    check_all_sequence(sqcs);
+    catch(const std::exception & expect)
+    {
+        std::cout << expect.what() << "\n";
+    }    
     return 0;
 }
